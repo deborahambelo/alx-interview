5392be84e1971b8c22678995419ae539925712d9
deborahambelo <ketema.deborah@gmail.com>
2024-07-31 18:50:08 +0000

    added file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,25 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
 def pascal_triangle(n):
+    """Returns a list of lists containing each row of the pascal's triangle"""
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    pascal_triangle = [0] * n
 
-    for i in range(1, n):
-        row = [1]
+    for i in range(n):
+        new_row = [0] * (i + 1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
 
-    return triangle
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
