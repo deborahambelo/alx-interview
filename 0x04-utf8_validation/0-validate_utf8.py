422f776986441c7232a528a93c5aaf3657550123
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2024-08-22 22:29:06 +0530

    Update 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,30 +1,34 @@
-def validUTF8(data):
-    """Checks if a given list of integers data set represents a valid UTF-8 encoding."""
-    
-    def is_continuation(byte):
-        """Check if a byte is a continuation byte (starts with 10)."""
-        return (byte & 0b11000000) == 0b10000000
+#!/usr/bin/python3
+"""
+validUTF8
+"""
 
-    i = 0
-    while i < len(data):
-        # Consider only the 8 least significant bits
-        first_byte = data[i] & 0xFF
-        
-        if first_byte >> 7 == 0:  # 1-byte character
-            i += 1
-        elif first_byte >> 5 == 0b110:  # 2-byte character
-            if i + 1 >= len(data) or not is_continuation(data[i+1] & 0xFF):
+
+def validUTF8(data):
+    """Determines if a given data set represents a valid UTF-8 encoding"""
+    count = 0
+
+    for x in data:
+
+        if 191 >= x >= 128:
+
+            if not count:
                 return False
-            i += 2
-        elif first_byte >> 4 == 0b1110:  # 3-byte character
-            if i + 2 >= len(data) or not is_continuation(data[i+1] & 0xFF) or not is_continuation(data[i+2] & 0xFF):
-                return False
-            i += 3
-        elif first_byte >> 3 == 0b11110:  # 4-byte character
-            if i + 3 >= len(data) or not is_continuation(data[i+1] & 0xFF) or not is_continuation(data[i+2] & 0xFF) or not is_continuation(data[i+3] & 0xFF):
-                return False
-            i += 4
+
+            count -= 1
         else:
-            return False
-    
-    return True
+            if count:
+                return False
+
+            if x < 128:
+                continue
+            elif x < 224:
+                count = 1
+            elif x < 240:
+                count = 2
+            elif x < 248:
+                count = 3
+            else:
+                return False
+
+    return count == 0
